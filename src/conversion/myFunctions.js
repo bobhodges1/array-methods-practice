be6e262da2459610d5e3578dfd0703c67c1d6139
Bob Hodges <bobhodges1@outlook.com>
2024-01-17 15:05:36 +0000

    all tests pass

--- src/conversion/myFunctions.js
+++ src/conversion/myFunctions.js
@@ -7,60 +7,88 @@
  * @param {string[]} inputStrings
  * @return {string|undefined}
  */
-function findFirstLongString(inputStrings) {}
+function findFirstLongString(inputStrings) {
+    const outputString = inputStrings.find(str => str.length >= 7)
+    return outputString;
+}
 
 /** return the first odd number in the given array of numbers, or undefined if none is found.
  * @param {number[]} inputNumbers
  * @return {number|undefined}
  */
-function findFirstOddNumber(inputNumbers) {}
+function findFirstOddNumber(inputNumbers) {
+    const outputNumber = inputNumbers.find(number => number % 2 === 1)
+    return outputNumber;
+} 
 
 /** return the LAST "long" string in the given array of strings, or undefined if none is found.
  * @param {string[]} inputStrings
  * @return {string|undefined}
  */
-function findLastLongString(inputStrings) {}
+function findLastLongString(inputStrings) {
+    const outputString = inputStrings.findLast(str => str.length >= 7)
+    return outputString;
+}
 
 /** Given an array of strings of various lengths,
  * return a new array containing only the "long" strings from the input array
  * @param {string[]} inputStrings
  * @returns {string[]}
  * */
-function findAllLongStrings(inputStrings) {}
+function findAllLongStrings(inputStrings) {
+    const outputStrings = inputStrings.filter(str => str.length >= 7)
+    return outputStrings;
+}
 
 /** given an array of numbers, return a new array containing all the odd numbers in the input array.  Returns an empty array if none are found.
  * @param {number[]} inputNumbers
  * @return {number[]}
  */
-function findAllOddNumbers(inputNumbers) {}
+function findAllOddNumbers(inputNumbers) {
+    const outputNumber = inputNumbers.filter(number => number % 2 === 1)
+    return outputNumber;
+}
 
 /** Given an array of mixed types of value
  * return a new array containing only those elements of type string.
  * @param {any[]} inputArray
  * @returns {string[]}
  * */
-function findAllTheStrings(inputArray) {}
+function findAllTheStrings(inputArray) {
+    const outputArray = inputArray.filter(element => typeof element ==='string')
+    return outputArray;
+}
 
 /** Given an array of people objects,
  * return a new array containing only those people objects whose firstName OR secondName (or both) is long.
  * @param {{firstName: string, secondName:string}[]} inputObjects
  * @returns {{firstName: string, secondName:string}[]}
  * */
-function findAllPeopleWithALongName(inputObjects) {}
+function findAllPeopleWithALongName(inputObjects) {
+    const outputObjects = inputObjects.filter(person => person.firstName.length >= 7 || person.secondName.length >= 7)
+    return outputObjects;
+}
 
 /** given an array of numbers return true if all of them are odd, else false.
  * @param {number[]} inputNumbers
  * @return {boolean}
  */
-function areAllNumbersOdd(inputNumbers) {}
+function areAllNumbersOdd(inputNumbers) {
+    const result = inputNumbers.every(number => number % 2 == 1)
+    return result;
+    }
+    
 
 /** Given an array of strings of various lengths,
  * return true if all of the strings are "long", else false
  * @param {string[]} inputStrings
  * @returns {boolean}
  * */
-function areAllStringsLong(inputStrings) {}
-
+function areAllStringsLong(inputStrings) {
+    const result = inputStrings.every(str => str.length >= 7)
+    return result;
+}
+    
 export {
     findFirstLongString,
     findFirstOddNumber,
